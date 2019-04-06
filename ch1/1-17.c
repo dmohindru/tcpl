92bdcac0b949af9d85557ef7319f6be39269243d
Dhruv Mohindru <dmohindru@gmail.com>
2019-04-06 09:35:46 +0530

    Solved ex 1-16, 1-17. Solving 1-18

--- ch1/1-17.c
+++ ch1/1-17.c
@@ -1,9 +1,10 @@
-/* Date: 05/04/19
-   Description: Print lenght of arbitrarily long input lines
+/* Date: 06/04/19
+   Description: Print line that are longer than 80 characters
 */
 #include <stdio.h>
 
 #define MAXLINE 1000    /* Maximum input line size */
+#define LONGLINE 80      /* Maximum limit for line to be printed */
 
 int mygetline(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -12,19 +13,11 @@ void copy(char to[], char from[]);
 main()
 {
     int len;        /* current line length */
-    int max;        /* maximum lenght seen so far */
     char line[MAXLINE];  /* current input line */
-    char longest[MAXLINE]; /* longest line saved here */
 
-    max = 0;
     while ((len = mygetline(line, MAXLINE)) > 0)
-        if (len > max) {
-            max = len;
-            copy(longest, line);
-        }
-    
-    if (max > 0)    /* there was a line */
-        printf("%d %s", max, longest);
+        if (len > LONGLINE)
+            printf("%s", line);
     
     return 0;
 }
@@ -32,21 +25,15 @@ main()
 /* getline: read a line into s, return length */
 int mygetline(char s[], int lim)
 {
-    int c, i, j;
-
-    j = 0;    
-    for (i=0; (c=getchar()) != EOF && c != '\n'; ++i)
-        if (i < lim-2) {
-            s[i] = c;
-            j++;
-        }
-        
+    int c, i;
+    
+    for (i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
     if (c == '\n') {
-        s[j] = c;
-        ++j;
+        s[i] = c;
         ++i;
     }
-    s[j] = '\0';
+    s[i] = '\0';
     return i;
 }
 
--- ch1/1-18.c
+++ ch1/1-18.c
@@ -1,5 +1,6 @@
 /* Date: 05/04/19
-   Description: Print lenght of arbitrarily long input lines
+   Description: Program to remove trailing blanks and tabs from each line
+                and delete entirely blank lines.
 */
 #include <stdio.h>
 
@@ -7,46 +8,33 @@
 
 int mygetline(char line[], int maxline);
 void copy(char to[], char from[]);
+int removeblanks(char s[]);
 
 /* Print longest input line */
 main()
 {
     int len;        /* current line length */
-    int max;        /* maximum lenght seen so far */
     char line[MAXLINE];  /* current input line */
-    char longest[MAXLINE]; /* longest line saved here */
 
-    max = 0;
+
     while ((len = mygetline(line, MAXLINE)) > 0)
-        if (len > max) {
-            max = len;
-            copy(longest, line);
-        }
-    
-    if (max > 0)    /* there was a line */
-        printf("%d %s", max, longest);
-    
+        if (removeblanks(line) > 0)
+            printf("%d %s", len, line);
     return 0;
 }
 
 /* getline: read a line into s, return length */
 int mygetline(char s[], int lim)
 {
-    int c, i, j;
-
-    j = 0;    
-    for (i=0; (c=getchar()) != EOF && c != '\n'; ++i)
-        if (i < lim-2) {
-            s[i] = c;
-            j++;
-        }
-        
+    int c, i;
+    
+    for (i=0; i < lim-1 && (c=getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
     if (c == '\n') {
-        s[j] = c;
-        ++j;
+        s[i] = c;
         ++i;
     }
-    s[j] = '\0';
+    s[i] = '\0';
     return i;
 }
 
@@ -59,4 +47,17 @@ void copy(char to[], char from[] )
     while ((to[i] = from[i]) != '\0')
         i++;
 
+}
+
+/* remove trailing blanks from line and return lenght of new line */
+int removeblanks(char s[])
+{
+    int i = 0;
+    /* find \n character */
+    while (s[i] != '\n')
+        ++i;
+    
+    /* move backwards till non blank character is found */
+
+    return i;
 }
