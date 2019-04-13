a64d090e5217f4071d93f1a2da31a048abcfbdd7
Dhruv Mohindru <dmohindru@gmail.com>
2019-04-13 08:09:37 +0530

    Test message

--- ch1/1-24.c
+++ ch1/1-24.c
@@ -4,36 +4,20 @@
    esacpe sequences and comments
 */
 #include <stdio.h>
+int comments();      /* Function to check comments block */
 
 main()
 {
     char c, nextc;
-    int multi_comments = 0; /* Multiple line comments */
-    int single_comments = 0; /* Single line comments */
 
     while ((c = getchar()) != EOF) {
-        if (c == '/' && multi_comments == 0 && single_comments == 0) {/* It maybe a start of comment block */ 
+        if (c == '/') {/* It maybe a start of comment block */ 
             nextc = getchar();
-            if (nextc == '*')  /* Its a start of comment block, mark its begining */
-                multi_comments = 1;
-            else if (nextc == '/') // Its a start of single line comment
-                single_comments = 1;
-            else { /* Its not a comment block print c and nextc */
-                putchar(c);
-                putchar(nextc);
+            if (nextc == '*') {  /* Its a start of comment block, mark its begining */
+
             }
         }
-        else if(c == '*' && multi_comments == 1) {/* It maybe a end of comment block */
-            nextc = getchar();
-            if (nextc == '/') /* Its a end of comment block, mark its end */
-                multi_comments = 0;    
-        }
-        else if(c == '\n' && single_comments == 1) { // Its a end of single line comment
-            putchar(c);
-            single_comments = 0;
-        }
-        else if (multi_comments == 0 && single_comments == 0)
-            putchar(c);
+        
     }
     return 0;
         
