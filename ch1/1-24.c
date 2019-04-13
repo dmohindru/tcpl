49f12eb1937bb3aaff1d2bf1f0cdcd837cd2a497
Dhruv Mohindru <dmohindru@gmail.com>
2019-04-13 16:46:19 +0530

    Attempted ex 1-24. Copied from The C Answer Book

--- ch1/1-24.c
+++ ch1/1-24.c
@@ -4,32 +4,91 @@
    esacpe sequences and comments
 */
 #include <stdio.h>
-int check_comment();      /* Function to check comments block, return 1 on valid comments, 0 otherwise */
 
+int braces, brack, paren;
+
+void in_quote(int c);
+void in_comment(void);
+void search();
+
+/* rudimentary syntax checker for C program */
 main()
 {
-    char c, nextc;
+    int c;
+    extern int braces, brack, paren;
 
-    while ((c = getchar()) != EOF) {
-        if (c == '/') {/* It maybe a start of comment block */ 
-            nextc = getchar();
-            if (nextc == '*') {  /* Its a start of comment block, mark its begining */
-
-            }
+    while((c = getchar()) != EOF) {
+        if (c == '/') {
+            if ((c = getchar()) == '*')
+                in_comment();   /* insdie comment */
+            else
+                search(c);
         }
+        else if (c == '\'' || c == '"')
+            in_quote(c);        /* inside quote */
+        else
+            search(c);
         
+        if (braces < 0) {
+            printf("Unbalanced braces\n");
+            braces = 0;
+        }
+        else if (brack < 0) {
+            printf("Unbalanced brackets\n");
+            brack = 0;
+        }
+        else if (paren < 0) {
+            printf("Unbalanced parentheses\n");
+            paren = 0;
+        }
     }
-    return 0;     
+
+    if (braces > 0)
+        printf("Unbalanced braces\n");
+    if (brack >0)
+        printf("Unbalanced brackets\n");
+    if (paren > 0)
+        printf("Unbalanced parentheses\n");
+        
 }
 
-
-/* Function checks for proper comment block */
-int check_comment()
+/* search: search for rudimentary syntax errors */
+void search(int c)
 {
-    char c;
-    int ret_val = 0; /* By default we assume it is a error */
-    while ((c=getchar()) != EOF) {
-        
-    }
+    extern int braces, brack, paren;
 
+    if (c == '{')
+        ++braces;
+    else if (c == '}')
+        --braces;
+    else if (c == '[')
+        ++brack;
+    else if (c == ']')
+        --brack;
+    else if (c == '(')
+        ++paren;
+    else if (c == ')')
+        --paren;
+}
+
+/* in_comment: inside of a valid comment */
+void in_comment()
+{
+    int c, d;
+    c= getchar();                   /* prev character */
+    d = getchar();                  /* curr character */
+    while (c != '*' || d != '/') {  /* search for end */
+        c = d;
+        d = getchar();
+    }
+}
+
+/* in_quote: inside quote */
+void in_quote(int c)
+{
+    int d;
+
+    while ((d = getchar()) != c)        /* search end quote */
+        if (d == '\\')                  /* igore escape seq */
+            getchar();
 }
