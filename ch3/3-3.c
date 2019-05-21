10f7370dc5bc0db5e1cae0e3a5542eb8f322c1f9
Dhruv Mohindru <dmohindru@gmail.com>
2019-05-21 13:05:30 +0530

    Solved ex 3-3

--- ch3/3-3.c
+++ ch3/3-3.c
@@ -7,13 +7,27 @@
 
 void expand(char s1[], char s2[])
 {
+    int i, j;
+    char c;
+    i = j = 0;
+    while ((c = s1[i++]) != '\0') {
+        if (s1[i] == '-' && s1[i+1] > c) {
+            while (c < s1[i+1])
+                s2[j++] = c++;
+            i += 1;
+        }
+        else
+            s2[j++] = c;
+    }
+    s2[j] = '\0';
     
 }
 
 int main()
 {
     int max = 100;
-    char s1[] = "a-z0-9";
+    //char s1[] = "a-z0-9";
+    char s1[] = "-a-b-c0-9";
     char s2[max];
     expand(s1, s2);
     printf("%s\n", s1);
