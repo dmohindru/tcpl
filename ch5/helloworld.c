5aa0de6a96338cdd2f3d8fcaa28f5f7dda26d2ec
Dhruv Mohindru <dmohindru@gmail.com>
2019-07-02 19:08:47 +0530

    Just giting so complete exercise

---
+++ ch5/helloworld.c
@@ -0,0 +1,16 @@
+/*
+ * helloworld.c (Self created excercise)
+ * Author       :   Dhruv Mohindru
+ * About        :   Self created exercise to play with command line argument
+ * Usage        :   hello -x -m <message>
+ * Date         :   July 7, 2019
+ * Description  :   This just a practice program with prints hello world message as per command line
+ *                  argument passed. If progam is supplied with command line argument -m <message> then
+ *                  <message> is printed on console otherwise default greeting message is printed. If -x
+ *                  argument is passed then extra default message is also printed along with greeting 
+ *                  message.
+ *                  User can conviently pass argument as -md rather than -m -d.
+ *                  Usage senario: hello -md hello_dhruv  
+ */
+
+#include <stdio.h>
